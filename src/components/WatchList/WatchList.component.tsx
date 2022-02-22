9c35f28afe45ce49844b73427ac1aba55af35e72
silpagui <silpagui.sa@gmail.com>
2022-02-22 22:48:06 +0100

    feat(WatchList.component.tsx): add link go back to home

--- src/components/WatchList/WatchList.component.tsx
+++ src/components/WatchList/WatchList.component.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { CartoonList } from "../CartoonList/CartoonList.component";
 
 export function WatchList() {
   return (
     <>
       <h1 className="page-title">Cartoons in my Watchlist</h1>
+      <Link to="/">Back to home</Link>
       <CartoonList favorites={true} />
     </>
   );
